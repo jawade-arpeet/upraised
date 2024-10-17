6e96e949efaefb4fcb3c30d0a2aed10e651ecf90
jawade.arpeet <jawade.arpeet@gmail.com>
2024-10-17 14:18:13 +0530

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Button from "./components/ui/button";
 function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center justify-between h-screen p-6 bg-gradient-to-t from-purple-400 to-white">
+      <Image
+        src="/upraised-logo.svg"
+        alt="Upraised Logo"
+        width={150}
+        height={150}
+      />
+      <div className="size-40 shadow-xl flex flex-col items-center justify-center rounded-full bg-white">
+        <span className="font-semibold text-3xl p-6 ">Quiz</span>
+      </div>
+      <Button className="w-full">Start</Button>
+    </div>
+  );
 }
 
 export default HomePage;
